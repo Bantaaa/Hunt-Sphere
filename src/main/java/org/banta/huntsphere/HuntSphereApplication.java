52da44d78567ef79df13e1fc825ced363f4790d0
Taha Amnay ALLAM <114312925+Bantaaa@users.noreply.github.com>
2024-11-29 11:03:22 +0100

    dependencies

---
+++ src/main/java/org/banta/huntsphere/HuntSphereApplication.java
@@ -0,0 +1,13 @@
+package org.banta.huntsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuntSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/banta/huntsphere/HuntSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.banta.huntsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
