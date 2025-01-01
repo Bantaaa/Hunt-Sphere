9b56d81fde975eb3b9d10a0f6b878bd9b8a7ba51
Taha Amnay ALLAM <114312925+Bantaaa@users.noreply.github.com>
2025-01-01 18:04:22 +0100

    authentication works

--- src/main/java/org/banta/huntsphere/service/security/PasswordValidator.java
+++ src/main/java/org/banta/huntsphere/service/security/PasswordValidator.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 @Component
 public class PasswordValidator {
     private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+!*=-])(?=\\S+$).{6,}$";
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
     public boolean isInvalid(String password) {
